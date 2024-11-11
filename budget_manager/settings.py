ab87a4b9b96ff8002e140486820fb7688c261352
Rodion <rodionbulgakov20@gmail.com>
2024-11-11 18:43:09 +0200

    add settings

--- budget_manager/settings.py
+++ budget_manager/settings.py
@@ -143,7 +143,7 @@ LOGOUT_REDIRECT_URL = 'accounts/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'statiсfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 MEDIA_ROOT = BASE_DIR / 'media'
