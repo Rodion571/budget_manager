bba89263ff509c40fa13b89dd22154a7ac90a744
Rodion <rodionbulgakov20@gmail.com>
2024-11-14 20:36:13 +0200

    add

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'is_staff', 'is_active',)
+    list_filter = ('is_staff', 'is_active',)
+    search_fields = ('email', 'username',)
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
