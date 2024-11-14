f285703d768716a832d5867c2e02b285242e3b66
Rodion <rodionbulgakov20@gmail.com>
2024-11-15 00:15:43 +0200

    add more settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import register, user_login, user_logout, home
+from django.contrib import admin
 
 app_name = 'accounts'
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('home/', home, name='home_content'),
+    path('admin/', admin.site.urls),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -32,6 +32,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import UserLoginForm
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import UserLoginForm
+
 def user_login(request):
     if request.method == 'POST':
         form = UserLoginForm(request, data=request.POST)
@@ -39,7 +43,10 @@ def user_login(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(request, username=username, password=password)
-            if user is not None:
+            if user is not None and user.is_superuser:
+                login(request, user)
+                return redirect('admin:index')  # Перенаправление на админку для суперпользователей
+            elif user is not None:
                 login(request, user)
                 return redirect('home_content')
             else:
